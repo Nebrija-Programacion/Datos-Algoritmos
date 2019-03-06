220f9fc941df9a32210d66a49fbdae50e7e3cd07
Rocio Ruiz Ruiz <rocioruizruiz@MacBook-Air-de-Rocio.local>
2019-03-06 10:21:06 +0100

    c

--- ejemplos/lista-completa/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#include "node.h"
-
-Node* getMiddleNode(Node* first, int size);
-Node *search(Node *first, Node *last, int dato, int size);
-
-#endif // FUNCTIONS_H
